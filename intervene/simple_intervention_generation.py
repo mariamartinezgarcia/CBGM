3475d28ab49b83f95765824540eddd46fd591d81
María Martínez-García <martinez-garcia@cs.uni-saarland.de>
2025-03-18 16:43:02 +0100

    correct plot

--- intervene/simple_intervention_generation.py
+++ intervene/simple_intervention_generation.py
@@ -134,8 +134,9 @@ def main(config):
     for i in range(num_imgs):
         title1 = 'Concept '+concept+' active'
         title2 = 'Concept '+concept+' inactive'
-        img1 = generated_active[i]*0.5 + 0.5
-        img2 = generated_inactive[i]*0.5 + 0.5
+        if dataset=='celeba':
+            img1 = generated_active[i]*0.5 + 0.5
+            img2 = generated_inactive[i]*0.5 + 0.5
         plot_images_side_by_side(img1, img2, title1=title1, title2=title2, wb=wb)
     
     # 4. Save noise vectors and prob vectors for reproducibility
