610c0685711855299c5e243c3f94678cb75e60d6
Sofia Molotkova <molotkova548@gmail.com>
2025-04-22 16:57:05 +0200

    deleted mpi3d dataset files

--- datasets/mpi3d.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-with np.load('./datasets/real.npz', mmap_mode='r') as data:
-    # Get a memory-mapped reference to just the 'images' array
-    images = data['images']
-    
-    # Get the shape to understand the data structure
-    print(f"Images shape: {images.shape}")
